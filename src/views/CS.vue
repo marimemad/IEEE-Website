c410936fac16107af8bf063e7611ac27f223e45c
Samy Nassar <samy0ayman@gmail.com>
2021-01-27 05:47:02 +0200

    Add committees components

--- src/views/CS.vue
+++ src/views/CS.vue
@@ -1,9 +1,28 @@
 <template>
   <div class="cs">
-    <h1>CS</h1>
+    <b-alert show>Default Alert</b-alert>
+    <about-us v-bind:data="data['about']" />
+    
   </div>
 </template>
 
 <script>
+import Database from '/database/database';
+import AboutUs from '@/components/committees/AboutUs';
+
+export default{
+
+  
+
+  components:{
+    'about-us':AboutUs
+
+  },
+  data(){
+    return{
+      data: Database.committees.cs
+    }
+  }
+}
 
 </script>
