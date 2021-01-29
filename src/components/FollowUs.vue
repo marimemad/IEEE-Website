514ae665369384ede33972e5b6c7cd7d6cdcb535
Mahmoud Ramadan <Mahmoud Abdallah>
2021-01-30 01:28:49 +0200

    adding Contact-us-page

---
+++ src/components/FollowUs.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="follow-us-container">
+      <strong class="follow-us-text">Follow Us</strong><br /><br />
+      <div class="follow-us-box">
+        <ul class="list-social-media-links">
+          <li class="list-inline-item">
+            <a href="https://www.facebook.com/IEEEZSB" target="_blank"
+              ><b-icon icon="facebook" font-scale="2.5"></b-icon
+            ></a>
+          </li>
+          &nbsp;&nbsp;
+          <li class="list-inline-item">
+            <a href="https://twitter.com/ieeezsb" target="_blank"
+              ><b-icon icon="twitter" font-scale="2.5"></b-icon
+            ></a>
+          </li>
+          &nbsp;&nbsp;
+          <li class="list-inline-item">
+            <a href="https://www.linkedin.com/company/ieeezsb/" target="_blank"
+              ><b-icon icon="linkedin" font-scale="2.5"></b-icon
+            ></a>
+          </li>
+          &nbsp;&nbsp;
+          <li class="list-inline-item">
+            <a href="mailto:info@ieee-zsb.org" target="_blank"
+              ><b-icon icon="envelope" font-scale="2.5"></b-icon
+            ></a>
+          </li>
+          &nbsp;
+        </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "FollowUs",
+};
+</script>
+
+<style scoped>
+.list-social-media-links{
+  padding:0px;
+}
+</style>
