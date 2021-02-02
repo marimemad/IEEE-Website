d42deeff8929bce008f96680f76da2a3689e18f3
Samy Nassar <samy0ayman@gmail.com>
2021-02-02 09:17:41 +0200

    Fix blank space in committees page

--- src/components/committees/ContactUs.vue
+++ src/components/committees/ContactUs.vue
@@ -9,12 +9,12 @@
             </b-row>
 
             <b-row class="my-5">
-                <b-col data-aos="zoom-in-right" class="right-border contact">
+                <b-col data-aos="fade-up" class="right-border contact">
                     <b-icon icon="geo-alt-fill" font-scale="3" class="icon mb-3"></b-icon>
                     <h5>address</h5>
                     <p>{{contact.address}}</p>
                 </b-col>
-                <b-col data-aos="zoom-in-left" class="contact">
+                <b-col data-aos="fade-up" class="contact">
                     <b-icon icon="envelope-fill" font-scale="3" class="icon mb-3"></b-icon>
                     <h5>email</h5>
                     <p>{{contact.email}}</p>
@@ -24,25 +24,25 @@
 
             <b-form @submit="onSubmit">
                 <b-row>
-                    <b-col class="mb-2" data-aos="zoom-in-right" sm="6">
+                    <b-col class="mb-2" data-aos="fade-up" sm="6">
                         <b-form-input v-model="form.name" type="text" id="name" placeholder="Your Name" required></b-form-input>
                     </b-col>
-                    <b-col class="mb-2" data-aos="zoom-in-left" sm="6">
+                    <b-col class="mb-2" data-aos="fade-up" sm="6">
                         <b-form-input v-model="form.email" type="email" id="email" placeholder="Your Email" required></b-form-input>
                     </b-col>
                 </b-row>
                 <b-row>
-                    <b-col class="mb-2" data-aos="zoom-in-up">
+                    <b-col class="mb-2" data-aos="fade-up">
                         <b-form-input v-model="form.subject" type="text" id="subject" placeholder="Subject" required></b-form-input>
                     </b-col>
                 </b-row>
                 <b-row>
-                    <b-col class="mb-2" data-aos="zoom-in-up">
+                    <b-col class="mb-2" data-aos="fade-up">
                         <b-form-textarea rows="3" max-rows="12" v-model="form.message" type="text" id="message" placeholder="Message" required></b-form-textarea>
                     </b-col>
                 </b-row>
 
-                <b-button data-aos="zoom-in" data-aos-duration="3000" class="mb-2" type="submit" variant="primary">Submit</b-button>
+                <b-button data-aos="fade-up" data-aos-duration="3000" class="mb-2" type="submit" variant="primary">Submit</b-button>
 
             </b-form>       
         </b-container>
