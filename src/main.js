f63aa6d5a5849e6935096e36be660a8774f91d66
AA-HAMZA <ahmed.rk30@gmail.com>
2021-03-23 18:38:36 +0200

    uncommented aos

--- src/main.js
+++ src/main.js
@@ -3,18 +3,17 @@ import App from './App.vue'
 import  {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
 import VueRouter from 'vue-router'
 import router from './router'
-/* import AOS from 'aos'; */
+import AOS from 'aos';
 
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import './plugins/bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-/* import 'aos/dist/aos.css'; */
+import 'aos/dist/aos.css';
 
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
-
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
@@ -23,9 +22,9 @@ Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
-/*   created() {
-    AOS.init({disable:'mobile', delay: 1000});
-  }, */
+  created() {
+    AOS.init({delay: 300});
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
