a3d765c372833afce274c6d5577b53987fdc4646
TheMidgetWidget <lightlord323@gmail.com>
2021-08-02 17:19:43 +0200

    Add color changes and some layout changes

--- src/views/Activities.vue
+++ src/views/Activities.vue
@@ -6,47 +6,49 @@
         <h2 class="activity-section-heading" data-aos="fade-in">Courses</h2>
         <div
           class="activity-cards"
-          data-aos="fade-right"
-          data-aos-offset="200"
-          data-aos-duration="300"
         >
           <b-row
             v-for="index in coursesData.length"
             :key="index"
           >
-              <b-col class="activity-list" md="12" lg="6">
+              <b-col class="activity-container" md="12" lg="6">
                 <Activity
                 :title="coursesData[index-1].title"
                 :description="
                   coursesData[index-1].description
                 "
                 :date="coursesData[index-1].date"
+
+                data-aos="fade-right"
+                data-aos-offset="200"
+                data-aos-duration="600"
               />
               </b-col>
           </b-row>
         </div>
       </b-container>
     </section>
+    <hr data-aos="flip-left" data-aos-duration="900">
     <section>
       <b-container class="activity-section">
         <h2 class="activity-section-heading" data-aos="fade-down">Workshops</h2>
-        <div class="activity-cards" data-aos="flip-right" data-aos-duration="800">
-          <div
+        <div class="activity-cards">
+          <b-row
             v-for="index in workshopData.length"
             :key="index"
           >
-            <b-col class="activity-list" sm="12" md="12" lg="6">
+            <b-col class="activity-container" md="12" lg="6">
               <Activity
-                class="activity"
                 :title="workshopData[index-1].title"
                 :description="
                   workshopData[index-1].description
                 "
                 :date="workshopData[index-1].date"
-                :backgroundColor="'#002855'"
+
+                data-aos="flip-right" data-aos-duration="800"
               />
             </b-col>
-          </div>
+          </b-row>
         </div>
       </b-container>
     </section>
@@ -100,8 +102,15 @@ export default {
 </script>
 
 <style scoped>
+hr {
+  border: 3px solid #002855;
+  width: 50%;
+  margin: 0 auto;
+  border-radius: 5px;
+}
+
 #activities {
-  background-color: #002855;
+  background-color: whitesmoke;
 }
 
 .activity-section {
@@ -109,59 +118,22 @@ export default {
   min-height: 100vh;
 }
 
-section:nth-child(odd) {
-  background-color: #007377;
-  box-shadow: 0px 2px 2px #005255, 0px -2px 2px #005255;
-}
-
 .activity-section-heading {
   text-align: center;
   margin-bottom: 20px;
   text-decoration-line: underline;
   text-decoration-style: solid;
-  text-decoration-color: #00b5e2;
+  text-decoration-color: #002855;
   text-decoration-thickness: 2px;
   text-underline-offset: 10px;
-  color: #FFF;
+  color: #002855;
 }
 
-.activity-list {
+.activity-container {
   margin: auto;
 }
 
 .activity {
   margin: 15px 0;
 }
-
-/* SCROLL ANIMATIONS
-.before-enter.horizontal-slide {
-  opacity: 0;
-  transform: translateX(-400px);
-  transition: all 0.5s linear;
-}
-
-.before-enter.vertical-slide-up {
-  opacity: 0;
-  transform: translateY(400px);
-  transition: all 0.3s linear;
-}
-
-.before-enter.vertical-slide-down {
-  opacity: 0;
-  transform: translateY(-100px);
-  transition: all 0.5s linear;
-}
-
-.before-enter.fade-in {
-  opacity: 0;
-  transition: all 0.5s linear;
-}
-
-.enter.horizontal-slide,
-.enter.vertical-slide-up,
-.enter.vertical-slide-down,
-.enter.fade-in {
-  opacity: 1;
-  transform: translateX(0px);
-} */
 </style>
